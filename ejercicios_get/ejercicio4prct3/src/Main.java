af6995c169430d62f2d5a84b83a2fe21d8e87602
Urtzi <urtzi.maruri.48440@ikasle.egibide.org>
2025-12-16 09:48:23 +0100

    ejercicio4

--- ejercicios_get/ejercicio4prct3/src/Main.java
+++ ejercicios_get/ejercicio4prct3/src/Main.java
@@ -14,7 +14,13 @@ public class Main {
         telefono.put("Jose", 977654321);
         telefono.put("Pepe", 987654821);
         telefono.put("Miguel", 987664321);
-
+        mostrarAgenda();
+    }
+    public static void mostrarAgenda() {
+        System.out.println("------AGENDA TELEFÓNICA------");
+        for (String key : telefono.keySet()) {
+            System.out.println("Nombre: " + key + " - Número: " + telefono.get(key));
+        }
         menuOpciones();
     }
 
@@ -23,7 +29,7 @@ public class Main {
         do {
             System.out.println("------MENU DE OPCIONES------");
             System.out.println("1. Agregar numero");
-            System.out.println("2. Buscar numero");
+            System.out.println("2. Borrar numero");
             System.out.println("3. Buscar numero");
             System.out.println("4. Borrar numero por el nombre");
             System.out.println("5. Buscar numero por el nombre");
@@ -73,10 +79,68 @@ public class Main {
         int numeroTelefono = Integer.parseInt(numero);
         telefono.put(nombre, numeroTelefono);
         System.out.println("Número agregado correctamente.");
+        System.out.println("Numero telefono: " + telefono.get(nombre));
+        System.out.println("Quieres agregar otro numero? (s/n)");
+        String respuesta = sc.nextLine();
+        if (respuesta.equalsIgnoreCase("s")) {
+            agregarNumero();
+        }else {
+            System.out.println("Volviendo al menú principal...");
+            menuOpciones();
+        }
     }
-    public static void borraNumero() {}
-    public static void buscarNumero() {}
-    public static  void borrarPorNombre() {}
-    public static void buscarPorNombre() {}
+    public static void borraNumero() {
+        System.out.print("¿Qué número desea borrar?: ");
+        int numeroBorrar = Integer.parseInt(sc.nextLine());
+        if (telefono.containsKey(numeroBorrar)) {
+            telefono.remove(numeroBorrar);
+            System.out.println("Número borrado correctamente.");
+            System.out.println("El numero de " + numeroBorrar + " ha sido eliminado.");
+        } else {
+            System.out.println("El numero no existe en la agenda.");
+        }
+        System.out.println("Quieres eliminar otro numero? (s/n)");
+        String respuesta = sc.nextLine();
+        if (respuesta.equalsIgnoreCase("s")) {
+            borraNumero();
+        }else {
+            System.out.println("Volviendo al menú principal...");
+            menuOpciones();
+        }
+    }
+    public static void buscarNumero() {
+        if (telefono.isEmpty()) {
+            System.out.println("La agenda está vacía.");
+            return;
+        }
+        System.out.print("¿Qué número desea buscar?: ");
+        int numeroBuscar = Integer.parseInt(sc.nextLine());
+        for (String key : telefono.keySet()) {
+            if (telefono.get(key) == numeroBuscar) {
+                System.out.println("Número encontrado: " + key + " - " + telefono.get(key));
+                return;
+            }
+        }
+    }
+    public static  void borrarPorNombre() {
+        System.out.println("El numero que desea borrar de quien es?");
+        String nombreBorrar = sc.nextLine();
+        if (telefono.containsKey(nombreBorrar)) {
+            telefono.remove(nombreBorrar);
+            System.out.println("Número borrado correctamente.");
+            System.out.println("El numero de " + nombreBorrar + " ha sido eliminado.");
+        } else {
+            System.out.println("El nombre no existe en la agenda.");
+        }
+    }
+    public static void buscarPorNombre() {
+        System.out.println("El numero que desea buscar de quien es?");
+        String nombreBuscar = sc.nextLine();
+        if (telefono.containsKey(nombreBuscar)) {
+            System.out.println("Número encontrado: " + nombreBuscar + " - " + telefono.get(nombreBuscar));
+        } else {
+            System.out.println("El nombre no existe en la agenda.");
+        }
+    }
 
 }
